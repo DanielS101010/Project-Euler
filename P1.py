9190c658d8d2d58c57efb8a61a3247b514e9b60c
Daniel <steinerdaniel010@gmail.com>
2023-11-15 18:24:52 +0100

    Implementet faster solution

--- P1.py
+++ P1.py
@@ -1,5 +1,15 @@
+import math
+# easy but slower solutiom
+target = 1000
 sum = 0
-for i in range(1,1000):
-    if (i % 3 == 0 or i%5 == 0):
+for i in range(target):
+    if (i % 3 == 0 or i % 5 == 0):
         sum += i
 print(sum)
+
+# better solution
+def sumDivisibleBy(n):
+    p = math.floor(target / n)
+    return n * (p * (p + 1)) / 2
+
+print(sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15))
