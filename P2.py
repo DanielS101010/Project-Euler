e74896cd93149d3eb439db226f6e43457ad432ba
Daniel <steinerdaniel010@gmail.com>
2023-11-16 12:07:08 +0100

    Implementet better solution

--- P2.py
+++ P2.py
@@ -1,10 +1,14 @@
-list = [1, 2]
-sum = 0
-while list[len(list)-1] <= 4000000:
-    list.append(list[len(list) - 2] + list[len(list)-1])
+limit = 4000000
 
-for i in list:
-    if i % 2 == 0:
-        sum += i
+a = 1
+b = 1
+sum_even_fib = 0
 
-print(sum)
+while b < limit:
+    if b % 2 == 0:
+        sum_even_fib += b
+    # Update the Fibonacci sequence correctly
+    a, b = b, a + b
+
+print(sum_even_fib)
+
